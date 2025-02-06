7801bc7435bdad6e71721a000faef8e89b622aaa
Robot-mitski <118906932+Robot-mitski@users.noreply.github.com>
2025-02-06 19:21:29 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+import { DotLottie } from '@lottiefiles/dotlottie-web';
+
+const dotLottie = new DotLottie({
+    autoplay: true,
+    loop: true,
+    canvas: document.querySelector('#dotlottie-canvas'),
+    src: "https://lottie.host/embed/51a78d9a-d9f2-48a7-a6dd-6057d0f47f43/dF25klVzN7.lottie", // replace with your .lottie or .json file URL
+});
